c5359b6544d4e15d93338712d54cc47f7747a3d5
minooaung <aungminoo83@gmail.com>
2024-02-03 11:31:42 +1300

    Initial Commit

---
+++ backend/app/Http/Resources/UserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    public static $wrap = false; // Turn Off wrapping data inside another data Eg. data.data
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        //return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'created_at' => $this->created_at->format('Y-m-d H:m:s')
+        ];
+    }
+}
